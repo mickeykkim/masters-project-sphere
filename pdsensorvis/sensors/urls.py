7284f8f644fd288b80959c85ae411472945ed4f3
mickeykkim <mickeykkim@gmail.com>
2019-06-19 15:04:05 +0100

    completed detail annotation view

--- pdsensorvis/sensors/urls.py
+++ pdsensorvis/sensors/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
    re_path(r'^patientdata/(?P<pk>\d+)$', views.PatientDataDetailView.as_view(), name='patientdata-detail'),
    path('wearabledata/', views.WearableDataListView.as_view(), name='wearabledata'),
    re_path(r'^wearabledata/(?P<pk>[-\w]+)$', views.WearableDataDetailView.as_view(), name='wearabledata-detail'),
-   # re_path(r'^wearabledata/(?P<pk>[-\w]+)/(?P<pk>\d+)$', views.WearableAnnotationDetailView.as_view(), name='wearableannotation-detail'),
+   re_path(r'^wearabledata/(?P<uuid>[-\w]+)/(?P<pk>\d+)$', views.WearableAnnotationDetailView.as_view(), name='wearableannotation-detail'),
    path('cameradata/', views.CameraDataListView.as_view(), name='cameradata'),
    re_path(r'^cameradata/(?P<pk>[-\w]+)$', views.CameraDataDetailView.as_view(), name='cameradata-detail'),
-   # re_path(r'^cameradata/(?P<pk>[-\w]+)/(?P<pk>\d+)$', views.CameraAnnotationDetailView.as_view(), name='cameraannotation-detail'),
+   re_path(r'^cameradata/(?P<uuid>[-\w]+)/(?P<pk>\d+)$', views.CameraAnnotationDetailView.as_view(), name='cameraannotation-detail'),
 ]
