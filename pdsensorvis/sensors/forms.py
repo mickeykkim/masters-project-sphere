832e297ee99ef8866a032b4f9d45f4db92929158
mickeykkim <mickeykkim@gmail.com>
2019-07-28 14:01:02 +0100

    added virtual environment and pysrt, xlwt

---
+++ pdsensorvis/sensors/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import WearableAnnotation, CameraAnnotation, UPDRS_TASK
+
+
+class WearableAnnotationForm(forms.ModelForm):
+   class Meta:
+      model = WearableAnnotation
+      fields=['annotation']
+
+
+class CameraAnnotationForm(forms.ModelForm):
+   class Meta:
+      model = CameraAnnotation
+      fields=['annotation']
+
